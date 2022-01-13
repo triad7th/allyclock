0e8dc2d75a19efbe0a78cf6f76dcd7b02053248e
Kevin Lee <wonseok.lee80@gmail.com>
2022-01-12 19:49:20 -0800

    Migrate from ti7clock #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'allyclock';
+  now: Date = new Date();
+  flags = {
+    "US": "https://cdn4.iconfinder.com/data/icons/rounded-square-national-flags/512/USA-128.png",
+    'UK': "https://cdn4.iconfinder.com/data/icons/rounded-square-national-flags/512/GREAT_BRITAIN-128.png",
+    'KR': "https://cdn1.iconfinder.com/data/icons/rounded-square-national-flags/512/SOUTH_KOREA-128.png"
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.now = new Date();
+    });
+  }
+
 }
