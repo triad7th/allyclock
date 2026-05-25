625f2d55bc1f0088ed8fbacd3b0d154d893dfd7f
Kevin Lee <wonseok.lee80@gmail.com>
2026-05-25 13:15:13 -0700

    chore(web): bump Angular 18 to 19

--- apps/web/src/app/card/card.component.ts
+++ apps/web/src/app/card/card.component.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
