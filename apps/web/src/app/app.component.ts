4e4341879de400a4c35b304b9257e9d73f0d48b2
Kevin Lee <wonseok.lee80@gmail.com>
2026-05-25 13:59:28 -0700

    chore(web): cleanup after Angular 21 upgrade

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent],
+  imports: [CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
